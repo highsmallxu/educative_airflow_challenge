0b11026deb30c6d826d5fc1d03bb528bbb390b43
highsmallxu <highsmallxu@gmail.com>
2023-07-23 22:15:02 +0200

    auth

--- dags/exchange_rate_reporting_pipeline.py
+++ dags/exchange_rate_reporting_pipeline.py
@@ -3,18 +3,26 @@ from airflow.decorators import dag, task
 from airflow.providers.google.cloud.operators.bigquery import \
     BigQueryInsertJobOperator
 from airflow.sensors.external_task import ExternalTaskSensor
-
+import json
+import os
 
 def get_execution_date_fn(logical_date, **kwargs):
     return kwargs["data_interval_end"]
 
+gcp_auth_key = <<gcp_auth>>
+
+with open('auth.json', 'w') as outfile:
+    json.dump(gcp_auth_key, outfile)
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "auth.json"
+PROJECT_ID = json.load(open("auth.json","rb"))["quota_project_id"]
 
 @dag(
     schedule="*/5 * * * *",
     start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
     catchup=False,
     tags=["educative"],
-    template_searchpath=["/opt/airflow/dags/sql"],
+    template_searchpath=["/usercode/sql"],
 )
 def exchange_rate_reporting_pipeline():
     sensor = ExternalTaskSensor(
@@ -27,6 +35,7 @@ def exchange_rate_reporting_pipeline():
 
     calculate_avg_rates = BigQueryInsertJobOperator(
         task_id="calculate_avg_rates",
+        project_id=PROJECT_ID,
         configuration={
             "query": {
                 "query": "{% include 'exchange_rate_report.sql' %}",
