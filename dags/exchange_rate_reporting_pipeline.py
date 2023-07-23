b004ef1cc5c8570c081439f36cf22619a98cffb6
highsmallxu <highsmallxu@gmail.com>
2023-07-23 21:52:29 +0200

    add solution

---
+++ dags/exchange_rate_reporting_pipeline.py
@@ -0,0 +1,41 @@
+import pendulum
+from airflow.decorators import dag, task
+from airflow.providers.google.cloud.operators.bigquery import \
+    BigQueryInsertJobOperator
+from airflow.sensors.external_task import ExternalTaskSensor
+
+
+def get_execution_date_fn(logical_date, **kwargs):
+    return kwargs["data_interval_end"]
+
+
+@dag(
+    schedule="*/5 * * * *",
+    start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["educative"],
+    template_searchpath=["/opt/airflow/dags/sql"],
+)
+def exchange_rate_reporting_pipeline():
+    sensor = ExternalTaskSensor(
+        task_id="sensor_exchange_rate_ingestion_pipeline",
+        external_dag_id="exchange_rate_ingestion_pipeline",
+        external_task_id="merge_temp_table_into_dest",
+        execution_date_fn=get_execution_date_fn,
+        timeout=300,
+    )
+
+    calculate_avg_rates = BigQueryInsertJobOperator(
+        task_id="calculate_avg_rates",
+        configuration={
+            "query": {
+                "query": "{% include 'exchange_rate_report.sql' %}",
+                "useLegacySql": False,
+            }
+        },
+    )
+
+    sensor >> calculate_avg_rates
+
+
+exchange_rate_reporting_pipeline()
---
+++ sql/exchange_rate.sql
@@ -0,0 +1,13 @@
+MERGE INTO airflow_challenge.exchange_rate AS dst
+USING (
+    SELECT timestamp, from_cur, to_cur, rate
+    FROM airflow_challenge.exchange_rate_staging
+) AS src
+ON dst.timestamp = src.timestamp
+AND dst.from_cur = src.from_cur
+AND dst.to_cur = src.to_cur
+WHEN MATCHED THEN
+    UPDATE SET dst.rate = src.rate
+WHEN NOT MATCHED THEN
+    INSERT (timestamp, from_cur, to_cur, rate)
+    VALUES (timestamp, from_cur, to_cur, rate)
