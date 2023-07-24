4e1a059ce53be5f1c3c9f7a7956c0245cf2590a8
highsmallxu <highsmallxu@gmail.com>
2023-07-24 20:53:05 +0200

    delete

--- setup/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python /usercode/setup/setup.py
-echo 'export GOOGLE_APPLICATION_CREDENTIALS=auth.json' >> ~/.bashrc
-echo 'export AIRFLOW_CONN_FRANKFURTER_API=http://https://api.frankfurter.app/' >> ~/.bashrc
-source ~/.bashrc
